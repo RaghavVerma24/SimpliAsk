9b635fb605a0076bab55a148d756bd83487fe424
Raghav <iraghav.me@gmail.com>
2023-03-26 13:46:43 -0400

    Minor css changes to landing page

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class=" bg-[#202454] rounded-lg shadow m-4 dark:bg-gray-800 mb-10">
+      <footer class=" bg-[#202454] rounded-lg shadow m-4 dark:bg-gray-800 mb-0 mx-auto mt-20 md:container md:mx-auto">
         <div class="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
           <span class="text-sm text-white sm:text-center dark:text-gray-400">
             © 2023{" "}
--- client/src/components/Landing/Components/Home.jsx
+++ client/src/components/Landing/Components/Home.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import DoctorImg from '../../../assets/doctor.svg';
+import DoctorImg from "../../../assets/doctor.svg";
 const Home = () => {
-  return (
-    <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div className="float-left">
-          <h2 className="text-[40px] font-bold text-white">The Ultimate Doctor to Patient connectivity tool</h2>     
-          <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">Facilitate secure and reliable communication between doctors and patients through a range of digital channels</h2>  
-          <div className="hidden space-x-2 md:inline-block">
-          <a
-            href="javascript:void(0)"
-            className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
-          >
-            Get Started
-          </a>
-        </div> 
-        </div>  
-        <div className="max-w-40">
-            <img src={DoctorImg} alt="Doctor Image " style={{width: 700 }}/>
+    return (
+      <div className="justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8 mb-10">
+            <div className="float-left mr-20 xl:max-w-3xl">
+                <h2 className="text-[40px] font-bold text-white">
+                    The Ultimate Doctor to Patient connectivity tool
+                </h2>
+                <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+                    Facilitate secure and reliable communication between doctors
+                    and patients through a range of digital channels, such as video conferencing, messaging, and email.
+                </h2>
+                <div className="hidden space-x-2 md:inline-block">
+                    <a
+                        href="javascript:void(0)"
+                        className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
+                    >
+                        Get Started
+                    </a>
+                </div>
+            </div>
+            <div className="max-w-50">
+                <img
+                    src={DoctorImg}
+                    alt="Doctor Image "
+                    style={{ width: 500 }}
+                />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Home
+export default Home;
