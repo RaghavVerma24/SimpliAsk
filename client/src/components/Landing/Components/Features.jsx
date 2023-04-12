91ceff8772290829784aa6b1b0c3d4d842206284
Raghav <iraghav.me@gmail.com>
2023-04-11 23:53:10 -0400

    Changed ui for landing page

--- client/src/components/Landing/Components/Features.jsx
+++ client/src/components/Landing/Components/Features.jsx
@@ -3,22 +3,41 @@ import DoctorImg from "../../../assets/doctor.svg";
 
 const Features = () => {
     return (
-        <div className="py-10 justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
-            {/* <div className="float-left max-w-50 mr-20">
-                <img
-                    src={DoctorImg}
-                    alt="Doctor Image "
-                    style={{ width: 500 }}
-                />
+        <>
+            <div className="py-10 bg-[#fff] justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
+                <div className="max-w-50">
+                    <img
+                        src={DoctorImg}
+                        alt="Doctor Image "
+                        style={{ width: 500 }}
+                    />
+                </div>
+                <div className="float-left ml-20 xl:max-w-3xl">
+                    <h2 className="text-[40px] font-bold text-white">
+                        The Ultimate Doctor to Patient connectivity tool
+                    </h2>
+                    <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+                        Facilitate secure and reliable communication between doctors
+                        and patients through a range of digital channels, such as video conferencing, messaging, and email.
+                    </h2>
+                    <div className="hidden space-x-2 md:inline-block">
+                        <a
+                            href="javascript:void(0)"
+                            className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
+                        >
+                            Get Started
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="xl:max-w-3xl">
+            <div className="py-10 justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
+            <div className="float-left mr-20 xl:max-w-3xl">
                 <h2 className="text-[40px] font-bold text-white">
                     The Ultimate Doctor to Patient connectivity tool
                 </h2>
                 <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
                     Facilitate secure and reliable communication between doctors
-                    and patients through a range of digital channels, such as
-                    video conferencing, messaging, and email.
+                    and patients through a range of digital channels, such as video conferencing, messaging, and email.
                 </h2>
                 <div className="hidden space-x-2 md:inline-block">
                     <a
@@ -28,13 +47,68 @@ const Features = () => {
                         Get Started
                     </a>
                 </div>
-            </div> */}
-            <div className="grid grid-cols-3 gap-20 break-words w-full xl:max-w-7xl">
-              <div className="bg-[#fff]">a</div>
-              <div className="bg-[#fff]">b</div>
-              <div className="bg-[#fff]">c</div>
-          </div>
+            </div>
+            <div className="max-w-50">
+                <img
+                    src={DoctorImg}
+                    alt="Doctor Image "
+                    style={{ width: 500 }}
+                />
+            </div>
         </div>
+        <div className="py-10 bg-[#fff] justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
+                <div className="max-w-50">
+                    <img
+                        src={DoctorImg}
+                        alt="Doctor Image "
+                        style={{ width: 500 }}
+                    />
+                </div>
+                <div className="float-left ml-20 xl:max-w-3xl">
+                    <h2 className="text-[40px] font-bold text-white">
+                        The Ultimate Doctor to Patient connectivity tool
+                    </h2>
+                    <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+                        Facilitate secure and reliable communication between doctors
+                        and patients through a range of digital channels, such as video conferencing, messaging, and email.
+                    </h2>
+                    <div className="hidden space-x-2 md:inline-block">
+                        <a
+                            href="javascript:void(0)"
+                            className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
+                        >
+                            Get Started
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="py-10 justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
+            <div className="float-left mr-20 xl:max-w-3xl">
+                <h2 className="text-[40px] font-bold text-white">
+                    The Ultimate Doctor to Patient connectivity tool
+                </h2>
+                <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+                    Facilitate secure and reliable communication between doctors
+                    and patients through a range of digital channels, such as video conferencing, messaging, and email.
+                </h2>
+                <div className="hidden space-x-2 md:inline-block">
+                    <a
+                        href="javascript:void(0)"
+                        className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
+                    >
+                        Get Started
+                    </a>
+                </div>
+            </div>
+            <div className="max-w-50">
+                <img
+                    src={DoctorImg}
+                    alt="Doctor Image "
+                    style={{ width: 500 }}
+                />
+            </div>
+        </div>
+        </>
     );
 };
 
--- client/src/components/Landing/Components/Home.jsx
+++ client/src/components/Landing/Components/Home.jsx
@@ -1,34 +1,53 @@
 import React from "react";
+import { faArrowCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import DoctorImg from "../../../assets/doctor.svg";
 const Home = () => {
     return (
-      <div className="py-10 justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
-            <div className="float-left mr-20 xl:max-w-3xl">
-                <h2 className="text-[40px] font-bold text-white">
-                    The Ultimate Doctor to Patient connectivity tool
-                </h2>
-                <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
-                    Facilitate secure and reliable communication between doctors
-                    and patients through a range of digital channels, such as video conferencing, messaging, and email.
-                </h2>
-                <div className="hidden space-x-2 md:inline-block">
-                    <a
-                        href="javascript:void(0)"
-                        className="px-8 py-4 text-white bg-[#202454] font-bold rounded-md shadow text-[18px]"
-                    >
-                        Get Started
-                    </a>
+        <>
+            <div className="py-10 justify-center xl:max-w-8xl mx-auto md:items-center flex flex-row md:px-8">
+                <div className="float-left ml-20 xl:max-w-3xl pr-20">
+                    <h2 className="text-[50px] font-bold text-white">
+                        The Ultimate Doctor to Patient connectivity tool
+                    </h2>
+                    <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+                        Facilitate secure and reliable communication between doctors
+                        and patients through a range of digital channels, such as video conferencing, messaging, and email.
+                    </h2>
+                    <div className="hidden space-x-2 md:inline-block mr-5 shadow-2xl">
+                        <a
+                            href="javascript:void(0)"
+                            className="px-8 py-4 text-white bg-[#6c63ff] font-bold rounded-md shadow text-[18px]"
+                        >
+                            Get Started
+                        </a>
+                    </div>
+                    <div className="hidden space-x-2 md:inline-block shadow-2xl">
+                        <a
+                            href="javascript:void(0)"
+                            className="px-8 py-4 text-[#191b3c] bg-white font-bold rounded-md shadow text-[18px]"
+                        >
+                            Learn More
+                        </a>
+                    </div>
                 </div>
+                <div className="max-w-50">
+                    <img
+                        src={DoctorImg}
+                        alt="Doctor Image "
+                        style={{ width: 800 }}
+                    />
+                </div>
+                
             </div>
-            <div className="max-w-50">
-                <img
-                    src={DoctorImg}
-                    alt="Doctor Image "
-                    style={{ width: 500 }}
+            <h1 className="animate-bounce text-white w-6 h-6 mx-auto mb-10">
+                <FontAwesomeIcon
+                    icon={faArrowCircleDown}
+                    className=" text-[50px]"
                 />
-            </div>
-        </div>
+            </h1>
+        </>
     );
 };
 
