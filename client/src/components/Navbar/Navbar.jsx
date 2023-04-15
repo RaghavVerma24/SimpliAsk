1ff7b89d1e236b8d14fbfc8938c5c3f3e4d0a1d2
Raghav <iraghav.me@gmail.com>
2023-04-15 15:25:04 -0400

    Added a fixed navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full bg-[#191b3c] shadow">
+    <nav className="sticky top-0 w-full bg-[#191b3c] shadow">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
