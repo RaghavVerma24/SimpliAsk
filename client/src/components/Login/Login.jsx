7be0fa362f2715768fdc26a864ec98e04a8ece56
Raghav <iraghav.me@gmail.com>
2023-04-15 23:00:06 -0400

    Changed UI for login and signup pages

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 import Navbar from "../Navbar/Navbar";
 import axios from "axios";
-import DoctorImg from "../../assets/doctor.svg";
+import LoginImg from "../../assets/login.svg";
+import User1 from "../../assets/users/user1.png"
+import User2 from "../../assets/users/user2.png"
+import User3 from "../../assets/users/user3.png"
+import User4 from "../../assets/users/user4.png"
 
 function Login() {
     const [email, setEmail] = useState("");
@@ -40,98 +44,68 @@ function Login() {
     return (
         <div className="">
             <Navbar />
-            <section className="gradient-form h-100vh bg-[#191b3c]">
+            <section className="gradient-form h-[93vh] bg-[#191b3c]">
                 <div className="container h-full p-10 m-auto">
-                    <div className="h-30 g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-                        <div className="h-30 w-full">
-                            <div className="h-30 block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-                                <div className="h-30 g-0 lg:flex lg:flex-wrap">
-                                    <div className="px-4 md:px-0 lg:w-6/12">
+                    <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+                        <div className="w-full">
+                            <div className="h-[65vh] block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                                <div className="h-full g-0 lg:flex lg:flex-wrap">
+                                    <div className="px-4 md:px-0 lg:w-1/2">
                                         <div className="md:mx-2 md:p-8">
-                                            <h1 className="text-2xl font-bold font-['Inter']">
-                                                Welcome Back
-                                            </h1>
-                                            <form action="POST">
-                                                <div
-                                                    className="relative mb-4"
-                                                    data-te-input-wrapper-init
-                                                >
-                                                    <input
-                                                        type="email"
-                                                        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                                        id="userEmail"
-                                                        placeholder="Email"
-                                                        onChange={(e) => {
-                                                            setEmail(
-                                                                e.target.value
-                                                            );
-                                                        }}
-                                                    />
-                                                    <label
-                                                        htmlFor="userEmail"
-                                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-                                                    >
-                                                        Email
-                                                    </label>
-                                                </div>
-                                                <div
-                                                    className="relative mb-4"
-                                                    data-te-input-wrapper-init
-                                                >
-                                                    <input
-                                                        type="password"
-                                                        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                                        id="userPassword"
-                                                        placeholder="Password"
-                                                        onChange={(e) => {
-                                                            setPassword(
-                                                                e.target.value
-                                                            );
-                                                        }}
-                                                    />
-                                                    <label
-                                                        htmlFor="userPassword"
-                                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-                                                    >
-                                                        Password
-                                                    </label>
-                                                </div>
-                                                <div className="mb-12 pt-1 pb-1 text-center">
-                                                    <button
-                                                        className="bg-[#191b3c] mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                                                        type="submit"
-                                                        data-te-ripple-init
-                                                        data-te-ripple-color="light"
-                                                        onClick={submit}
-                                                    >
-                                                        Log in
-                                                    </button>
-                                                    <a href="#!">
-                                                        Forgot password?
-                                                    </a>
-                                                </div>
-                                                <div className="flex items-center justify-between pb-6">
-                                                    <p className="mb-0 mr-2">
-                                                        Don't have an account?
-                                                    </p>
-                                                    <button
-                                                        type="button"
-                                                        className="inline-block rounded border-2 border-danger px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                                                        data-te-ripple-init
-                                                        data-te-ripple-color="light"
-                                                    >
-                                                        Register
-                                                    </button>
-                                                </div>
+                                            <h1 className="text-2xl md:text-3xl font-bold leading-tight">Welcome Back!</h1>
+                                            <form className="mt-5" action="POST">
+                                            <div>
+                                                <label className="block text-gray-700">Email Address</label>
+                                                <input type="email" name id placeholder="Enter Email Address" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autoComplete required onChange={(e) => {setEmail(e.target.value);}}/>
+                                            </div>
+                                            <div className="mt-4">
+                                                <label className="block text-gray-700">Password</label>
+                                                <input type="password" name id placeholder="Enter Password" minLength={6} className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required onChange={(e) => {setPassword(e.target.value);}}/>
+                                            </div>
+                                            <div className="text-right mt-2">
+                                                <a href="#" className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+                                            </div>
+                                            <button type="submit" className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                                                px-4 py-3 mt-6" onClick={submit}>Log In</button>
                                             </form>
+                                            <hr className="my-6 border-gray-300 w-full" />
+                                            <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+                                            <div className="flex items-center justify-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" /></defs><clipPath id="b"><use xlinkHref="#a" overflow="visible" /></clipPath><path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" /><path clipPath="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" /><path clipPath="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" /><path clipPath="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" /></svg>
+                                                <span className="ml-4">
+                                                Log in
+                                                with
+                                                Google
+                                                </span>
+                                            </div>
+                                            </button>
+                                            <p className="mt-8">
+                                            Need an account? 
+                                            
+                                            <a href="#" className="text-blue-500 hover:text-blue-700 font-semibold ml-1">
+                                                Create an account
+                                            </a>
+                                            </p>
+                                            <div className="flex">
+                                                <div className="flex my-5 -space-x-4">
+                                                    <img className="w-10 h-10 border-2 border-[#191b3c] rounded-full dark:border-gray-800" src={User1} alt="f" />
+                                                    <img className="w-10 h-10 border-2 border-[#191b3c] rounded-full dark:border-gray-800" src={User2} alt="" />
+                                                    <img className="w-10 h-10 border-2 border-[#191b3c] rounded-full dark:border-gray-800" src={User3} alt="" />
+                                                    <img className="w-10 h-10 border-2 border-[#191b3c] rounded-full dark:border-gray-800" src={User4} alt="" />
+                                                </div>
+                                                <div class="bg-gray-700 w-0.5 h-7 my-auto ml-3"></div>
+                                                <h1 className="float-left ml-3 my-auto text-black">Join <span className="font-bold">Hundreds</span> of Happy Clients</h1>
+                                            </div>
+                                            <p className="text-sm text-gray-500 mt-5">© 2023 SimpliAsk - All Rights Reserved.</p>
                                         </div>
                                     </div>
-                                    <div>
+                                    <div className="px-4 md:px-0 lg:w-1/2 bg-[#FD7401] rounded-r-lg">
                                         <img
-                                            src={DoctorImg}
+                                            src={LoginImg}
                                             alt="Doctor Image "
-                                            style={{ width: 600 }}
+                                            className="w-full p-10"
                                         />
+                                            
                                     </div>
                                 </div>
                             </div>
