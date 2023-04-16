8119b6c0cad89c6cd99c19006a1e7971611ebfed
Raghav <iraghav.me@gmail.com>
2023-04-16 16:56:25 -0400

    Outlined Appointments page in Dashboard

--- client/src/pages/Appointments/Appointments.jsx
+++ client/src/pages/Appointments/Appointments.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import DashboardHeader from '../Dashboard/DashboardHeader'
+import Sidebar from '../Sidebar/Sidebar'
+import AddAppointment from './AddAppointments'
+import Calendar from './Calendar'
 
 const Appointments = () => {
   return (
-    <div>Appointments</div>
+    <div className="bg-[#191b3c]" >
+      <DashboardHeader/> 
+      <div className="flex bg-white h-screen">
+        <div className="float-left">
+            <Sidebar/>
+        </div>
+        <div className="bg-[#fff] w-full">
+          <div className="grid grid-cols-2 gap-4 h-full bg-[#fff]">
+            <AddAppointment/>
+            <div className="float-left">
+              <Calendar/>
+              {/* <AddAppointment/> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
