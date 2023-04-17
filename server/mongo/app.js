dc274be26a639f000ca3aae4ed4cfa7a18d9cf0c
Raghav <iraghav.me@gmail.com>
2023-04-16 22:55:19 -0400

    Setup express server endpoint for appointments

--- server/mongo/app.js
+++ server/mongo/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const collection = require("./mongo");
+const {collection, appointCollection} = require("./mongo.js");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+var UUID;
 
 app.get("/login", cors(), (req, res) => {});
 
@@ -16,6 +18,8 @@ app.post("/login", async (req, res) => {
         
         if (check && check.password == password) {
             res.json("exist");
+            UUID = (check._id).toString();
+            console.log(req.body, UUID)
         } else {
             res.json("nonexist");
         }
@@ -33,7 +37,7 @@ app.post("/signup", async (req, res) => {
         email: email,
         password: password,
     };
-    
+    console.log(req.body)
 
     try {
         const check = await collection.findOne({ email: email });
@@ -48,6 +52,39 @@ app.post("/signup", async (req, res) => {
         res.json("nonexist");
     }
 });
+
+app.get("/appointments", cors(), (req, res) => {});
+
+app.post("/appointments", async (req, res) => {
+    if (req.body.type == "create") {
+        const { status, doctorName, patientName, aptDate, aptTime, aptNotes } = req.body;
+
+        const data = {
+            userId: UUID,
+            doctorName: doctorName,
+            patientName: patientName,
+            aptDate: aptDate,
+            aptTime: aptTime,
+            aptNotes: aptNotes,
+        };
+
+        try {
+            await appointCollection.insertMany([data]); 
+            res.json({ data: data})
+        } catch (e) {
+            console.log(e)
+        }
+    } else if (req.body.type == "calendar") {
+        try {
+            let appointments = await appointCollection.find({ userId: UUID})
+            console.log(appointments, UUID)
+            res.json({ data: appointments})
+        } catch {
+
+        }
+    }
+});
+
 
 app.listen(8000, () => {
     console.log("Port Connected");
