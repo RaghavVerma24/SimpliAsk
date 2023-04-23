f4b41e063f07463f5a9c8c61d2686c1fb74aff37
Raghav <iraghav.me@gmail.com>
2023-04-23 12:37:52 -0400

    Added functionality to contact form

--- client/src/components/Landing/Components/Contact.jsx
+++ client/src/components/Landing/Components/Contact.jsx
@@ -7,26 +7,21 @@ const Contact = () => {
         <section className="text-gray-800">
           <div className="flex flex-wrap">
             <div className="grow-0 shrink-0 basis-auto mb-6 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
-              <h2 className="text-[30px] font-bold text-[#fff] mb-5">
-                Contact Us
+              <h2 className="text-[40px] font-bold text-[#fff]">
+                  Contact Us
               </h2>
-              <p className="text-gray-500 mb-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laudantium, modi accusantium ipsum corporis quia asperiores
-                dolorem nisi corrupti eveniet dolores ad maiores repellendus enim
-                autem omnis fugiat perspiciatis? Ad, veritatis.
-              </p>
-              <p className="text-gray-500 mb-2">New York, 94126, United States</p>
-              <p className="text-gray-500 mb-2">+ 01 234 567 89</p>
-              <p className="text-gray-500 mb-2">info@gmail.com</p>
+              <h2 className="text-[20px] text-[#c0b2ac] break-word mb-10">
+              We value your health and well-being, and our team of experienced healthcare professionals are here to assist you. Whether you have questions about our services, need to schedule an appointment, or have any other inquiries, we're here to help. You can reach us by filling out the contact form.
+                    </h2>
+                    
             </div>
             <div className="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
-              <form>
+              <form action="mailto:racooraghav24@gmail.com" method="post" enctype="text/plain">
                 <div className="form-group mb-6">
                   <input type="text" className="form-control block
               w-full
               px-3
-              py-1.5
+              py-3
               text-base
               font-normal
               text-gray-700
@@ -42,7 +37,7 @@ const Contact = () => {
                   <input type="email" className="form-control block
               w-full
               px-3
-              py-1.5
+              py-3
               text-base
               font-normal
               text-gray-700
@@ -73,29 +68,8 @@ const Contact = () => {
               focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
             " id="exampleFormControlTextarea13" rows={3} placeholder="Message" defaultValue={""} />
                 </div>
-                <div className="form-group form-check text-center mb-6">
-                  <input type="checkbox" className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer" id="exampleCheck87" defaultChecked />
-                  <label className="form-check-label inline-block text-white" htmlFor="exampleCheck87">Send me a copy of this
-                    message</label>
-                </div>
                 <button type="submit" className="
-            w-full
-            px-6
-            py-2.5
-            bg-blue-600
-            text-white
-            font-medium
-            text-xs
-            leading-tight
-            uppercase
-            rounded
-            shadow-md
-            hover:bg-blue-700 hover:shadow-lg
-            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg
-            transition
-            duration-150
-            ease-in-out">Send</button>
+            px-4 py-2 text-white bg-[#6469ff] rounded-md shadow w-full text-[20px]">Send</button>
               </form>
             </div>
           </div>
